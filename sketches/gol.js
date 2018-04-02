97428686e13646a53a1c7d6905656a5d882ead93
Ryan Nguyen <xoflippzxo@yahoo.com>
2018-04-02 15:07:31 -0700

    Finished step 2, track cells in grid. Also updated step one by dividing width and height by cellSize.

--- sketches/gol.js
+++ sketches/gol.js
@@ -9,16 +9,34 @@ function draw () {
   background(250);
 
    grid.draw();
+
+
 }
 
 class Grid {
   constructor (cellSize) {
-    // update the contructor to take cellSize as a parameter
-    // use cellSize to calculate and assign values for numberOfColumns and numberOfRows
+
+    /* indentifys the cell size */
     this.cellSize = cellSize;
-    this.numberOfColumns = width
-    this.numberOfRows =  height
-  }
+    /*calculates the width of the columns while keeping the constant resolution of the cell size */
+    this.numberOfColumns = width / cellSize;
+    /* calculates the height of the columns while keeping the constant resolution of the cell size */
+    this.numberOfRows =  height / cellSize;
+
+    /* how big the first array should be */
+    var x = this.numberOfColumns; 
+    /* how big each array inside of the first array should be */
+    var y = this.numberOfRows;
+    /* create the initial array */
+    var cells = new Array(x);
+    /* loop over each position in the array */
+    /* create another array inside of the first array at the position 'i' */
+    for (var i = 0; i < cells.length; i ++) {
+      cells[i] = new Array(y); //
+    }
+    /* indicates the cells in the console */
+    print (cells);
+}
 
   draw () {
     for (var column = 0; column < this.numberOfColumns; column++) {
@@ -29,4 +47,5 @@ class Grid {
       }
     }
   }
+
 }
